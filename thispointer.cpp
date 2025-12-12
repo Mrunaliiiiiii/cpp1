999020163c72b8055a8979ca9d605c64ca7bdc63
Mrunu <mrunali.lavangare1@gmail.com>
2025-12-12 04:03:50 +0000

    thispointer.cpp

---
+++ thispointer.cpp
@@ -0,0 +1,30 @@
+#include <iostream>  
+using namespace std;  
+class Employee {  
+   public:  
+       int id;    
+       string name;  
+       float salary;  
+       Employee(int id, string name, float salary)    
+        {    
+            this->id = id;    
+           this->name = name;    
+           this->salary = salary;   
+        }    
+       void display()    
+        {   
+
+            cout<<id<<"  "<<name<<"  "<<salary<<endl;    
+            cout<<"Address :"<<this<<endl;
+        }    
+};  
+int main(void) {  
+    Employee e1 =Employee(101, "DEEPAK", 1000000);   
+    Employee e2=Employee(102, "MIKEY", 69);   
+    e1.display();    
+    
+    cout<<&e1<<endl;
+    cout<<&e2<<endl;
+    e2.display();    
+    return 0;  
+}
