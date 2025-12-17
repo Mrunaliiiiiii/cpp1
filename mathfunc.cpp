503c3a7fd3331798fbed998102b3dcdfeb29d466
Mrunu <mrunali.lavangare1@gmail.com>
2025-12-17 04:10:36 +0000

    mathhfunc.cpp

---
+++ mathfunc.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  
+#include<cmath>  
+using namespace std;  
+int main()  
+{  
+int base=9;  
+int exponent=4;  
+int power=pow(base,exponent);  
+std::cout << "Power of a given number is :" <<power;  
+return 0;  
+} 
