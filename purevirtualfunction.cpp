e0e303f198c79ee91b032cdd8f1a21f901542aa8
Mrunu <mrunali.lavangare1@gmail.com>
2025-12-29 04:30:41 +0000

    purevirtualfunction.cpp

---
+++ purevirtualfunction.cpp
@@ -0,0 +1,24 @@
+#include <iostream>    
+using namespace std;  //using standard namespace  
+class Base  //Abstract base class  
+{    
+    public:  //Access modifier  
+    virtual void show() = 0;   //Pure Virtual function  
+};    
+class Derived : public Base    
+{    
+    public:    
+    void show()    
+    {    
+        std::cout << "Derived class is derived from the base class." << std::endl;    
+    }    
+};    
+int main()  //Main Function  
+{    
+    Base *bptr;  //base class pointer  
+    //Base b;    
+    Derived d;    
+    bptr = &d;    
+    bptr->show();    
+    return 0;    
+}
